9775f95229e50c5b490ac15c03e14a58e18cc23d
Youngho-kr <Yyoung0997@gmail.com>
2024-02-15 16:59:30 +0900

    rename: Banner -> Header

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Banner from "./Banner";
+import Header from "./Header";
 import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   return (
     <>
-      <Banner />
+      <Header />
       <Outlet />
       <Footer />
     </>
