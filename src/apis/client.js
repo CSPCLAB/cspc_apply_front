f6ab72e05c378b831ab0119aae8f1d15a7f35ae0
Youngho-kr <Yyoung0997@gmail.com>
2024-02-20 16:31:13 +0900

    test: dev.cspc.me api

--- src/apis/client.js
+++ src/apis/client.js
@@ -1,5 +1,8 @@
 import axios from "axios";
-const API_SERVER_URL = "https://apply.cspc.me/api";
+// const API_SERVER_URL = "https://apply.cspc.me/api";
+
+/* 개발용 API */
+const API_SERVER_URL = "https://dev.cspc.me/api";
 
 export const Client = axios.create({
   baseURL: API_SERVER_URL,
