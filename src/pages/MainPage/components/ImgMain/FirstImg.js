cb4aac2caca49bba9543b9c3fe9019d41f8d3f90
ljy2855 <ljy2855@naver.com>
2024-02-22 23:54:55 +0900

    feat: Add main page api call

--- src/pages/MainPage/components/ImgMain/FirstImg.js
+++ src/pages/MainPage/components/ImgMain/FirstImg.js
@@ -1,17 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import img_1 from '../../../../assets/images/main/main_img_1.png'
 
 import GoToApply from '../../../../components/GoToApply'
 import { Link } from 'react-router-dom'
 import '../styles/ImgMain.css'
+import { get_recruit_info } from 'apis/get_recruit'
 
 const FirstImg = () => {
+    const [recruitTitle, setRecruitTitle] = useState('') // 상태로 제목을 관리합니다.
+
+    useEffect(() => {
+        // 컴포넌트가 마운트될 때 API를 호출합니다.
+        const fetchData = async () => {
+            try {
+                const data = await get_recruit_info() // API 호출
+                const termString =
+                    data.term === 'spring'
+                        ? '1학기'
+                        : data.term === 'fall'
+                        ? '2학기'
+                        : ''
+                // 완성된 문자열을 상태에 설정
+                setRecruitTitle(`${data.year} ${termString} 신입 부원 모집`)
+            } catch (error) {
+                console.error('API 호출 중 오류 발생:', error)
+            }
+        }
+
+        fetchData()
+    }, []) // 빈 배열을 의존성 배열로 제공하여 컴포넌트가 마운트될 때만 실행되도록 합니다.
     return (
         <div className="ImgMainBox">
             <img src={img_1} className="ImgMainStyle" />
             <div className="ImgMainLetterBox_1 WhiteWord ImgMain_1_Title">
-                2023 1학기 신입 부원 모집
+                {recruitTitle}
             </div>
             <div
                 className="GoToApplyStyle"
