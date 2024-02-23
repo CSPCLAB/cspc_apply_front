536ad4b889e4bb6f4428171146d59454a49ad5dd
darami819 <roadangel2002@gmail.com>
2024-02-23 14:09:17 +0900

    Mobile : ResultPage DocumentPass (이미지 문제 빼고)

--- src/mobile/ResultPage/ResultPage/components/DocumentPass.js
+++ src/mobile/ResultPage/ResultPage/components/DocumentPass.js
@@ -9,14 +9,14 @@ import dateFormat, { masks } from "dateformat";
 const Doc_result_pass = ({ resume }) => {
   const format = "m월 dd일 오후 h시 MM분";
   return (
-    <div className="black">
-      <div className="pass__resultbox">
-        <div className="pass__resultlen">서류결과</div>
+    <div className="black_">
+      <div className="pass__resultbox_">
+        <div className="pass__resultlen_">서류결과</div>
       </div>
-      <div className="pass__line"></div>
-      <div className="pass__resultbox2">
-        <div className="pass__resultlen1">{resume.name} 님 축하드립니다!</div>
-        <div className="pass__resultlen2">
+      <div className="pass__line_"></div>
+      <div className="pass__resultbox2_">
+        <div className="pass__resultlen1_">{resume.name} 님 축하드립니다!</div>
+        <div className="pass__resultlen2_">
           서류에 합격하셨습니다.
           <br />
           함께 하기까지 한 단계, 면접 전형이 남아 있습니다.
@@ -24,8 +24,8 @@ const Doc_result_pass = ({ resume }) => {
           전형 진행을 위해 면접 일정 및 장소를 안내드립니다.
           <br />
         </div>
-        <div className="pass__resultlen3">면접 안내</div>
-        <div className="pass__resultlen4">
+        <div className="pass__resultlen3_">면접 안내</div>
+        <div className="pass__resultlen4_">
           면접 시간은 <b>{dateFormat(resume.fixed_interview_time, format)}</b>
           입니다.
           <br />
@@ -34,8 +34,8 @@ const Doc_result_pass = ({ resume }) => {
           문의사항이 있으시다면 아래 이메일로 문의해주세요.
           <br />
         </div>
-        <div className="pass__picline">
-          <img src={imgMap} />
+        <div className="pass__picline_">
+          <img style={{ transform: 'scale(0.5)' }} src={imgMap} />
           <div className="pass__picline_spot">
             <img src={imgSpot} />
           </div>
