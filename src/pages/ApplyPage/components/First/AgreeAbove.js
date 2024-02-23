c20b7a62fc1817398851a567ad19215ff4557543
Youngho-kr <Yyoung0997@gmail.com>
2024-02-23 15:27:35 +0900

    feat: ApplyFirst responsive

--- src/pages/ApplyPage/components/First/AgreeAbove.js
+++ src/pages/ApplyPage/components/First/AgreeAbove.js
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 import GoWriteApply from "../GoWriteApply";
 
 import "../styles/AgreeAbove.css";
+import { isBrowser, isMobile } from "react-device-detect";
+import { Col, Container, Row } from "react-bootstrap";
 
 const AgreeAbove = ({ setPage }) => {
   const [checked, setChecked] = useState(0);
@@ -17,22 +19,58 @@ const AgreeAbove = ({ setPage }) => {
     else setChecked(0);
   };
 
-  return (
-    <div className="AgreeAboveCheck">
-      <div>
-        {isChecked() ? (
-          <button className="checked Button" onClick={changeChecked}></button>
-        ) : (
-          <button className="unChecked Button" onClick={changeChecked}></button>
-        )}
+  if (isBrowser) {
+    return (
+      <div className="AgreeAboveCheck">
+        <div>
+          {isChecked() ? (
+            <button className="checked Button" onClick={changeChecked}></button>
+          ) : (
+            <button
+              className="unChecked Button"
+              onClick={changeChecked}
+            ></button>
+          )}
+        </div>
+        <div className="AgreeAboveWord">위 사항에 동의합니다.</div>
+        <div className="GoWriteApplyButton">
+          {/* isChecked() 가 1일 때만 링크 이동할 수 있게 하기 */}
+          <GoWriteApply isChecked={isChecked()} first setPage={setPage} />
+        </div>
       </div>
-      <div className="AgreeAboveWord">위 사항에 동의합니다.</div>
-      <div className="GoWriteApplyButton">
-        {/* isChecked() 가 1일 때만 링크 이동할 수 있게 하기 */}
-        <GoWriteApply isChecked={isChecked()} first setPage={setPage} />
-      </div>
-    </div>
-  );
+    );
+  } else if (isMobile) {
+    return (
+      <Container fluid className="MobileAgreeAboveCheck">
+        <Row>
+          <Col xs={1}>
+            {isChecked() ? (
+              <button
+                className="checked MobileButton"
+                onClick={changeChecked}
+              ></button>
+            ) : (
+              <button
+                className="unChecked MobileButton"
+                onClick={changeChecked}
+              ></button>
+            )}
+          </Col>
+          <Col xs={11}>
+            <div className="MobileAgreeAboveWord">위 사항에 동의합니다.</div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className="MobileGoWriteApplyButton">
+              {/* isChecked() 가 1일 때만 링크 이동할 수 있게 하기 */}
+              <GoWriteApply isChecked={isChecked()} first setPage={setPage} />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 };
 
 export default AgreeAbove;
