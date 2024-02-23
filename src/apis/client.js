bcad1258f825bdf0f2815543779cefb559dc14e6
ljy2855 <ljy2855@naver.com>
2024-02-24 00:30:32 +0900

    fix: change prod API server

--- src/apis/client.js
+++ src/apis/client.js
@@ -1,9 +1,9 @@
-import axios from "axios";
+import axios from 'axios'
 // const API_SERVER_URL = "https://apply.cspc.me/api";
 
 /* 개발용 API */
-const API_SERVER_URL = "https://dev.cspc.me/api";
+const API_SERVER_URL = 'https://apply.cspc.me/api'
 
 export const Client = axios.create({
-  baseURL: API_SERVER_URL,
-});
+    baseURL: API_SERVER_URL,
+})
